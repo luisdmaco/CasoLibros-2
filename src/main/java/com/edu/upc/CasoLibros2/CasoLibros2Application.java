c9831dccde448c9696ab5b80adc221cacc47a3bc
Luis D. Maco <luisdmaco@outlook.com>
2023-06-15 22:44:54 -0500

    first commit

---
+++ src/main/java/com/edu/upc/CasoLibros2/CasoLibros2Application.java
@@ -0,0 +1,13 @@
+package com.edu.upc.CasoLibros2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoLibros2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoLibros2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/upc/CasoLibros2/CasoLibros2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.upc.CasoLibros2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoLibros2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
